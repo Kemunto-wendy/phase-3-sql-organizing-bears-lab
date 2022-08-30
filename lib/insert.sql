2bd3f454b100aa28df109912f1bd1e0866a42759
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-08-30 11:37:40 +0300

    changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 8, "F", "black", "carefree", "true");
+INSERT INTO bears VALUES (2,"Rowdy", 5, "F", "light", "Quiet", "false");
+INSERT INTO bears VALUES (3,"Tabitha", 6, "M", "turn", "Calm", "false");
+INSERT INTO bears VALUES (4,"Sergeant Brown", 9, "F", "grey", "Fat", "true");
+INSERT INTO bears VALUES (5,"Melissa", 3, "M", "dark brown", "goofy", "true");
+INSERT INTO bears VALUES (6,"Grinch", 2, "F", "dim", "Grinchy", "false");
+INSERT INTO bears VALUES (7,"Wendy", 9, "M", "dark", "naive", "true");
+INSERT INTO bears VALUES (8,null, 8, "F", "brown", "peaceful", "false");
